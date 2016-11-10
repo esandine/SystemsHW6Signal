f9035dce97b983a4f53f4b9922076d979fdd9b30
csherling <csherling@stuy.edu>
2016-11-09 21:07:19 -0500

    signals done

--- signals.c
+++ signals.c
@@ -2,17 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fcntl.h>
+
+#include <sys/stat.h>
+
 static void sighandler(int signo){
-  printf("Signal Caught");
+
+  if(signo == SIGINT){
+    int fd = open("kills.txt", O_CREAT | O_RDWR | O_APPEND, 0664);
+    char msg[] = "Exit due to SIGINT\n";
+    write(fd, msg, sizeof(msg));
+    close(fd);
+    printf("Caught a SIGINT\n");
+    exit(0);
+  }
+  else if (signo == SIGUSR1) {
+    printf("ppid: %d\n", getppid());
+  }
 }
 // signal(SIGINT,sighandler(SIGINT));
 
 int main(){
+  umask(0000);
+  signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
+  
   while(1){
     printf("This program is running\n");
     printf("PID: %d\n",getpid());
     sleep(1);
-    signal(SIGINT,sighandler(SIGINT));
   }
 }
 
